6051fabcfb2aaed7f0e5f8f8c6d85145ac76a407
Larisa Gota <lgota@smallfootprint.com>
2020-02-04 08:14:06 +0200

    add junit reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -133,10 +133,13 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec', ['allure', {
-        outputDir: 'allure-results',
-        disableWebdriverStepsReporting: true,
-        disableWebdriverScreenshotsReporting: false,
+    // reporters: ['spec', ['allure', {
+    //     outputDir: 'allure-results',
+    //     disableWebdriverStepsReporting: true,
+    //     disableWebdriverScreenshotsReporting: false,
+    // }]],
+    reporters: ['spec', ['junit', {
+        outputDir: 'allure-results'
     }]],
  
     //
